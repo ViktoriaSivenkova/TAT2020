78c09fed62a93df16f60282ec4cff0316ee32a8f
ViktoriaSivenkova <girlss4111@gmail.com>
2020-02-27 16:36:22 +0300

    Inital commit

---
+++ Dev1/Dev1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Dev1
+{
+    public class Program
+    {
+        /// <summary>
+        /// Entry point.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            var unitedString = string.Join(" ", args);
+            var workWithString = new WorkWithString();
+            var maxNumber = workWithString.GetNumberOfMaxRepeatingSymbols(unitedString);
+            Console.WriteLine(maxNumber);
+        }
+    }
+}
---
+++ Dev1/Dev1/WorkWithString.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dev1
+{
+    /// <summary>
+    /// Class for the work with the strings.
+    /// </summary>
+    public class WorkWithString
+    {
+        /// <summary>
+        /// Method which gets the number of 
+        /// </summary>
+        /// <returns></returns>
+        public int GetNumberOfMaxRepeatingSymbols(string inputString)
+        {
+            if (string.IsNullOrWhiteSpace(inputString) == true)
+            {
+                throw new Exception("Incorrect string");
+            }
+
+            int numberOfLetters = 1;
+            var listNumberOfLetters = new List<int>();
+
+            for (int i = 1; i < inputString.Length; i++)
+            {
+                if (inputString[i] == inputString[i - 1])
+                {
+                    numberOfLetters += 1;
+                    if (inputString[i] == inputString[inputString.Length-1])
+                    {
+                        listNumberOfLetters.Add(numberOfLetters);
+                    }
+
+                }
+                else
+                {
+                    listNumberOfLetters.Add(numberOfLetters);
+                    numberOfLetters = 1;
+                }
+            }
+
+            return inputString.Length == 1 ? 1 : listNumberOfLetters.Max(); // If string has only one any symbol - return 1
+        }
+    }
+}
