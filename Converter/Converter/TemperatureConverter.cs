617f77fb841f18465aa0368938d003fbf1a9d8fc
ViktoriaSivenkova <girls4111@gmail.com>
2020-04-24 21:03:41 +0300

    Add converter for lengh and temperature

---
+++ Converter/Converter/TemperatureConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Converter
+{
+    public class TemperatureConverter : BaseConverter
+    {
+        public TemperatureConverter(double number, string converterType) : base(number, converterType)
+        {
+
+        }
+        public double ConverterCelsiusToPharyngate()
+        {
+            return ((9.0 / 5.0) * Number) + 32;
+        }
+
+        public double ConverterPharyngateToCelsius()
+        {
+            return (5.0 / 9.0) * (Number - 32);
+        }
+
+        public override void ChoiceConverterType(string type)
+        {
+            switch (type.ToUpper())
+            {
+                case "CP":
+                    Result = ConverterCelsiusToPharyngate();
+                    break;
+                case "PC":
+                    Result = ConverterPharyngateToCelsius();
+                    break;               
+            }
+        }
+    }
+}
